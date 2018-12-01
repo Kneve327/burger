d8028e99fb55b4ed7ad781ef2a7fca32c37c02e3
Kneve327 <kneve327@gmail.com>
2018-12-01 08:52:23 -0500

    deploying to heroku

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+--   Inserted a set of records into the table
+  INSERT INTO burgers (burger_name, devoured) VALUES ("Double Bacon Cheeseburger", 0);
+  INSERT INTO burgers (burger_name, devoured) VALUES ("Vegie Burger", 0);
+  INSERT INTO burgers (burger_name, devoured) VALUES ("Breakfast All Day Burger", 0);
